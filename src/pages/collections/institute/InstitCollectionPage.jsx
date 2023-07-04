a3c91aaeab2580580050f2a282b2b4f113e2b146
Johnny Mcswiney <johnnymcswiney64@gmail.com>
2023-07-03 18:53:01 -0600

    Update InstitCollectionPage.jsx

--- src/pages/collections/institute/InstitCollectionPage.jsx
+++ src/pages/collections/institute/InstitCollectionPage.jsx
@@ -19,3 +19,142 @@ function InstitCollectionPage() {
 
 
 export default InstitCollectionPage;
+
+
+
+// import React, { useState } from 'react';
+// import { useParams } from 'react-router-dom';
+// import institutionsData from '../tempAssets/tempInstit.json';
+// import ImageContainer from '../../../components/containers/ImageContainer';
+// import InstHomeColl_ImageContainer from './InstColl_ImageContainer';
+// import './style.css';
+
+// function InstitCollectionPage() {
+//   const { id, collectionId } = useParams();
+//   const [institutions, setInstitutions] = useState(institutionsData.institutions);
+
+//   return (
+//     <div className='instit--collhomepage--main--cont'>
+//       {institutions.map((institution) => {
+//         if (institution.id === id) {
+//           if (collectionId) {
+//             // Render content for a specific collection
+//             const collection = institution.collections.find(
+//               (coll) => coll.id === collectionId
+//             );
+//             if (id && collectionId) {
+//               return (
+//                 <div className='instit--collhomepage--cont' key={institution.id}>
+//                   <div className='instit--collhomepage--title--cont'>
+//                     <div className='instit--collhomepage--title--img'>
+//                       <ImageContainer
+//                         imageUrl={institution.imgurl}
+//                         aspectRatio={3 / 3}
+//                       />
+//                     </div>
+//                     {institution.name}
+//                   </div>
+//                   <div className='instit--collhomepage--content--cont'>
+//                     {collectionId}
+//                     {/* <div
+//                       className='browse--instit--collection--card--1'
+//                       key={collection.id}
+//                     >
+//                       <InstHomeColl_ImageContainer
+//                         imageUrl={collection.imgurl}
+//                         collName={collection.name}
+//                         parentid={institution.id}
+//                         collectionid={collection.id}
+//                       />
+//                     </div>
+//                     {collection.description} */}
+//                   </div>
+//                 </div>
+//               );
+//             } else {
+//               // Handle case when collectionId is not found
+//               return (
+//                 <div className='instit--collhomepage--cont' key={institution.id}>
+//                   <div className='instit--collhomepage--title--cont'>
+//                     <div className='instit--collhomepage--title--img'>
+//                       <ImageContainer
+//                         imageUrl={institution.imgurl}
+//                         aspectRatio={3 / 3}
+//                       />
+//                     </div>
+//                     {institution.name}
+//                   </div>
+//                   <div className='instit--collhomepage--content--cont'>
+//                     Collection not found.
+//                   </div>
+//                 </div>
+//               );
+//             }
+//           } else {
+//             // Render content for the institute's home page
+//             return (
+//               <div className='instit--collhomepage--cont' key={institution.id}>
+//                 <div className='instit--collhomepage--title--cont'>
+//                   <div className='instit--collhomepage--title--img'>
+//                     <ImageContainer
+//                       imageUrl={institution.imgurl}
+//                       aspectRatio={3 / 3}
+//                     />
+//                   </div>
+//                   {institution.name}
+//                 </div>
+//                 <div className='instit--collhomepage--content--cont'>
+
+//                   {institution.collections.map(collection => (
+//                   <div
+//                     className='browse--instit--collection--card--1'
+//                     key={collection.id}
+//                   >
+//                     <InstHomeColl_ImageContainer
+//                       imageUrl={collection.imgurl}
+//                       collName={collection.name}
+//                       parentid={institution.id}
+//                       collectionid={collection.id}
+//                     />
+//                   </div>
+//                 ))}
+//                 {institution.description}
+                    
+
+//                 </div>
+//               </div>
+//             );
+//           }
+//         }
+//         return null;
+//       })}
+//     </div>
+//   );
+// }
+
+// export default InstitCollectionPage;
+
+
+
+
+
+
+
+
+
+
+// {institution.collections.map(collection => (
+//   <div
+//     className='browse--instit--collection--card--1'
+//     key={collection.id}
+//   >
+//     <InstHomeColl_ImageContainer
+//       imageUrl={collection.imgurl}
+//       collName={collection.name}
+//       parentid={institution.id}
+//       collectionid={collection.id}
+//     />
+//   </div>
+// ))}
+// {institution.description}
+//   </div>
