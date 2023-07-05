99b652a39385b1b23a7e4e78ca7640f862ba43a7
Johnny Mcswiney <johnnymcswiney64@gmail.com>
2023-07-04 23:13:03 -0600

    Now instit collections are fully displayed when clicked on ( i <3 chatgpt)

--- src/pages/collections/institute/InstitCollectionPage.jsx
+++ src/pages/collections/institute/InstitCollectionPage.jsx
@@ -1,160 +1,90 @@
-import React, { useState } from 'react';
-import { useParams } from 'react-router-dom';
-import institutionsData from '../tempAssets/tempInstit.json';
-import ImageContainer from '../../../components/containers/ImageContainer';
-import InstHomeColl_ImageContainer from './InstColl_ImageContainer';
-import './style.css';
+import React, { useState } from 'react'
+import { useParams } from 'react-router-dom'
+import institutionsData from '../tempAssets/tempInstit.json'
+import artifactsData from '../tempAssets/tempArtifacts.json'
+import ImageContainer from '../../../components/containers/ImageContainer'
+import InstHomeColl_ImageContainer from './InstColl_ImageContainer'
+import './style.css'
 
 function InstitCollectionPage() {
-  const { param1, param2 } = useParams();
+  const { param1, param2 } = useParams()
+  const [institutions, setInstitutions] = useState(institutionsData.institutions)
+  const [artifacts, setArtifacts] = useState(artifactsData.collections)
 
   if (param2) {
-    // Render content when param2 is defined
-    return <div>Param1: {param1}, Param2: {param2}</div>;
-  } else {
-    // Render content when param2 is not defined
-    return <div>Param1: {param1}</div>;
+    const collection = artifacts.find((coll) => coll.id === param2)
+
+    if (collection) {
+      return (
+        <div className='instit--collhomepage--main--cont'>
+          <div className='instit--collhomepage--cont'>
+            <div className='instit--collhomepage--title--cont'>
+              <div className='instit--collhomepage--title--img'>
+                <ImageContainer
+                  imageUrl={collection.imgurl}
+                  aspectRatio={3 / 3}
+                />
+              </div>
+              {collection.name}
+            </div>
+            <div className='instit--collhomepage--content--cont'>
+              {collection.artifacts.map((artifact) => (
+                <div
+                  className='browse--instit--collection--card--1'
+                  key={artifact.id}
+                >
+                  <InstHomeColl_ImageContainer
+                    imageUrl={artifact.imgurl}
+                    collName={artifact.name}
+                    parentid={collection.id}
+                    collectionid={artifact.id}
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      )
+    }
+  } else if (param1) {
+    const institution = institutions.find((inst) => inst.id === param1)
+
+    if (institution) {
+      return (
+        <div className='instit--collhomepage--main--cont'>
+          <div className='instit--collhomepage--cont'>
+            <div className='instit--collhomepage--title--cont'>
+              <div className='instit--collhomepage--title--img'>
+                <ImageContainer
+                  imageUrl={institution.imgurl}
+                  aspectRatio={3 / 3}
+                />
+              </div>
+              {institution.name}
+            </div>
+            <div className='instit--collhomepage--content--cont'>
+              {institution.description}
+              {institution.collections.map((collection) => (
+                <div
+                  className='browse--instit--collection--card--1'
+                  key={collection.id}
+                >
+                  <InstHomeColl_ImageContainer
+                    imageUrl={collection.imgurl}
+                    collName={collection.name}
+                    parentid={institution.id}
+                    collectionid={collection.id}
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      )
+    }
   }
+
+  return null // If neither param1 nor param2 is provided, return null or display an appropriate message
 }
 
-
-export default InstitCollectionPage;
-
-
-
-// import React, { useState } from 'react';
-// import { useParams } from 'react-router-dom';
-// import institutionsData from '../tempAssets/tempInstit.json';
-// import ImageContainer from '../../../components/containers/ImageContainer';
-// import InstHomeColl_ImageContainer from './InstColl_ImageContainer';
-// import './style.css';
-
-// function InstitCollectionPage() {
-//   const { id, collectionId } = useParams();
-//   const [institutions, setInstitutions] = useState(institutionsData.institutions);
-
-//   return (
-//     <div className='instit--collhomepage--main--cont'>
-//       {institutions.map((institution) => {
-//         if (institution.id === id) {
-//           if (collectionId) {
-//             // Render content for a specific collection
-//             const collection = institution.collections.find(
-//               (coll) => coll.id === collectionId
-//             );
-//             if (id && collectionId) {
-//               return (
-//                 <div className='instit--collhomepage--cont' key={institution.id}>
-//                   <div className='instit--collhomepage--title--cont'>
-//                     <div className='instit--collhomepage--title--img'>
-//                       <ImageContainer
-//                         imageUrl={institution.imgurl}
-//                         aspectRatio={3 / 3}
-//                       />
-//                     </div>
-//                     {institution.name}
-//                   </div>
-//                   <div className='instit--collhomepage--content--cont'>
-//                     {collectionId}
-//                     {/* <div
-//                       className='browse--instit--collection--card--1'
-//                       key={collection.id}
-//                     >
-//                       <InstHomeColl_ImageContainer
-//                         imageUrl={collection.imgurl}
-//                         collName={collection.name}
-//                         parentid={institution.id}
-//                         collectionid={collection.id}
-//                       />
-//                     </div>
-//                     {collection.description} */}
-//                   </div>
-//                 </div>
-//               );
-//             } else {
-//               // Handle case when collectionId is not found
-//               return (
-//                 <div className='instit--collhomepage--cont' key={institution.id}>
-//                   <div className='instit--collhomepage--title--cont'>
-//                     <div className='instit--collhomepage--title--img'>
-//                       <ImageContainer
-//                         imageUrl={institution.imgurl}
-//                         aspectRatio={3 / 3}
-//                       />
-//                     </div>
-//                     {institution.name}
-//                   </div>
-//                   <div className='instit--collhomepage--content--cont'>
-//                     Collection not found.
-//                   </div>
-//                 </div>
-//               );
-//             }
-//           } else {
-//             // Render content for the institute's home page
-//             return (
-//               <div className='instit--collhomepage--cont' key={institution.id}>
-//                 <div className='instit--collhomepage--title--cont'>
-//                   <div className='instit--collhomepage--title--img'>
-//                     <ImageContainer
-//                       imageUrl={institution.imgurl}
-//                       aspectRatio={3 / 3}
-//                     />
-//                   </div>
-//                   {institution.name}
-//                 </div>
-//                 <div className='instit--collhomepage--content--cont'>
-
-//                   {institution.collections.map(collection => (
-//                   <div
-//                     className='browse--instit--collection--card--1'
-//                     key={collection.id}
-//                   >
-//                     <InstHomeColl_ImageContainer
-//                       imageUrl={collection.imgurl}
-//                       collName={collection.name}
-//                       parentid={institution.id}
-//                       collectionid={collection.id}
-//                     />
-//                   </div>
-//                 ))}
-//                 {institution.description}
-                    
-
-//                 </div>
-//               </div>
-//             );
-//           }
-//         }
-//         return null;
-//       })}
-//     </div>
-//   );
-// }
-
-// export default InstitCollectionPage;
-
-
-
-
-
-
-
-
-
-
-// {institution.collections.map(collection => (
-//   <div
-//     className='browse--instit--collection--card--1'
-//     key={collection.id}
-//   >
-//     <InstHomeColl_ImageContainer
-//       imageUrl={collection.imgurl}
-//       collName={collection.name}
-//       parentid={institution.id}
-//       collectionid={collection.id}
-//     />
-//   </div>
-// ))}
-// {institution.description}
-//   </div>
+export default InstitCollectionPage
