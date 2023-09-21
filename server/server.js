487cd902def2e56efe082d4d1cf9064af4ce34fe
TRBLshoot <Dylanovandermeer@gmail.com>
2023-09-21 13:56:29 -0600

    Server.js fixes

--- server/server.js
+++ server/server.js
@@ -17,8 +17,7 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once('open', () => {
   console.log('Connected to MongoDB using Mongoose');
-
+  app.use(express.json())
   app.use('/api/user', userRoute);
-
   app.use('/api/collection', collectionRoute);
 });
