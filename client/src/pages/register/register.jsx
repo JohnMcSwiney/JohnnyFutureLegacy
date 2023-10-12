59218ba18ee2d07034c36375b533ca736baa72b1
Johnny Mcswiney <102752692+JohnMcSwiney@users.noreply.github.com>
2023-10-12 17:12:12 -0600

    changes

--- client/src/pages/register/register.jsx
+++ client/src/pages/register/register.jsx
@@ -62,143 +62,146 @@ function Register() {
         //button enabled with js hack
         const v1 = USER_REGEX.test(user);
         const v2 = PWD_REGEX.test(pwd);
-        if(!v1 || !v2) {
+        if (!v1 || !v2) {
             setErrMsg('invalid Entry');
             return;
         }
-
+        console.log(user, pwd);
+        setSuccess(true);
     }
     return (
-        <div>
-            <p ref={errRef}
-                className={errMsg ? "errMsg" : "offscreen"}
-                aria-live="assertive">
-                {errMsg}
-            </p>
-            <h1>Register</h1>
-            <form onSubmit={handleSubmit}>
-                <div className='input--cont'>{/* UserName */}
-                    <aside id="uidnote"
-                        className={userFocus && user && !validName ? "instructions" : "offscreen"}>
-                        <AiOutlineInfoCircle />
-                        4 to 24 characters. <br />
-                        Must begin with a letter.<br />
-                        Letters, numbers, underscores, hyphens allowed.
-                    </aside>
+        <>
+            {success ? (
+                <div>
+                    <h1>Success!</h1>
+                    <a className="register--link" onClick={handleRedirectLogin}>Login</a>
+                </div>
+            ) : (
+                <div>
+                    <p ref={errRef}
+                        className={errMsg ? "errMsg" : "offscreen"}
+                        aria-live="assertive">
+                        {errMsg}
+                    </p>
+                    <h1>Register</h1>
+                    <form onSubmit={handleSubmit}>
+                        <div className='input--cont'>{/* UserName */}
+                            <aside id="uidnote"
+                                className={userFocus && user && !validName ? "instructions" : "offscreen"}>
+                                <AiOutlineInfoCircle />
+                                4 to 24 characters. <br />
+                                Must begin with a letter.<br />
+                                Letters, numbers, underscores, hyphens allowed.
+                            </aside>
+                            <section>
+                                <label htmlFor="username">
+                                    Username:
+                                    <span className={validName ? 'valid' : 'hide'}>
+                                        <FaCheck />
+                                    </span>
+                                    <span className={validName || !user ? 'hide' : 'invalid'}>
+                                        <FaTimes />
+                                    </span>
+                                </label>
+                                <input
+                                    type="text"
+                                    id="username"
+                                    ref={userRef}
+                                    autoComplete='off'
+                                    onChange={(e) => setUser(e.target.value)}
+                                    required
+                                    aria-invalid={validName ? "false" : "true"}
+                                    aria-describedby="uidnote"
+                                    onFocus={() => setUserFocus(true)}
+                                    onBlur={() => setUserFocus(false)}
+                                />
+                            </section>
+                        </div>{/* UserName/ */}
+                        <div className='input--cont'>{/* Password */}
+                            <aside id="pwdnote"
+                                className={pwdFocus && !validPwd ? "instructions" : "offscreen"}>
+                                <AiOutlineInfoCircle />
+                                8 to 24 characters.
+                                Must include uppercase & lowercase letters, a number, & a special character.<br />
+                                <div className='special--chars'>
+                                    Allowed special characters:
+                                    <span aria-label="exclamation mark">!</span>
+                                    <span aria-label="at symbol">@</span>
+                                    <span aria-label="hashtag">#</span>
+                                    <span aria-label="dollar sign">$</span>
+                                    <span aria-label="percent">%</span>
+                                </div>
 
-                    <section>
-                        <label htmlFor="username">
-                            Username:
-                            <span className={validName ? 'valid' : 'hide'}>
-                                <FaCheck />
-                            </span>
-                            <span className={validName || !user ? 'hide' : 'invalid'}>
-                                <FaTimes />
-                            </span>
-                        </label>
-                        <input
-                            type="text"
-                            id="username"
-                            ref={userRef}
-                            autoComplete='off'
-                            onChange={(e) => setUser(e.target.value)}
-                            required
-                            aria-invalid={validName ? "false" : "true"}
-                            aria-describedby="uidnote"
-                            onFocus={() => setUserFocus(true)}
-                            onBlur={() => setUserFocus(false)}
-                        />
-                    </section>
+                            </aside>
+                            <section>
+                                <label htmlFor="password">
+                                    Password:
+                                    <span className={validPwd ? 'valid' : 'hide'}>
+                                        <FaCheck />
+                                    </span>
+                                    <span className={validPwd || !pwd ? 'hide' : 'invalid'}>
+                                        <FaTimes />
+                                    </span>
+                                </label>
 
+                                <input
+                                    type="password"
+                                    id="password"
+                                    onChange={(e) => setPwd(e.target.value)}
+                                    required
+                                    aria-invalid={validPwd ? "false" : "true"}
+                                    aria-describedby="pwdnote"
+                                    onFocus={() => setPwdFocus(true)}
+                                    onBlur={() => setPwdFocus(false)}
+                                />
 
+                            </section>
 
+                        </div>{/* Password/ */}
+                        <div className='input--cont'>{/* Password Match */}
+                            <aside id="confirmnote"
+                                className={matchFocus && !validMatch ? "instructions" : "offscreen"}>
+                                <AiOutlineInfoCircle />
+                                Must match the first password input field.
+                            </aside>
+                            <section>
+                                <label htmlFor="confirm_pwd">
+                                    Confirm Password:
+                                    <span className={validMatch && matchPwd ? 'valid' : 'hide'}>
+                                        <FaCheck />
+                                    </span>
+                                    <span className={validMatch || !matchPwd ? 'hide' : 'invalid'}>
+                                        <FaTimes />
+                                    </span>
+                                </label>
 
+                                <input
+                                    type="password"
+                                    id="confirm_pwd"
+                                    onChange={(e) => setMatchPwd(e.target.value)}
+                                    required
+                                    aria-invalid={validMatch ? "false" : "true"}
+                                    aria-describedby="confirmnote"
+                                    onFocus={() => setMatchFocus(true)}
+                                    onBlur={() => setMatchFocus(false)}
+                                />
 
-                </div>{/* UserName/ */}
-                <div className='input--cont'>{/* Password */}
-                    <aside id="pwdnote"
-                        className={pwdFocus && !validPwd ? "instructions" : "offscreen"}>
-                        <AiOutlineInfoCircle />
-                        8 to 24 characters.
-                        Must include uppercase & lowercase letters, a number, & a special character.<br />
-                        <div className='special--chars'>
-                            Allowed special characters:
-                            <span aria-label="exclamation mark">!</span>
-                            <span aria-label="at symbol">@</span>
-                            <span aria-label="hashtag">#</span>
-                            <span aria-label="dollar sign">$</span>
-                            <span aria-label="percent">%</span>
+                            </section>
+
+                        </div>{/* Password Match/ */}
+                        <div>
+                            <button disabled={!validName || !validPwd || !validMatch ? true : false}>Sign Up</button>
+                        </div>
+                        <div>
+                            Already registered?<br />
+                            <a className="register--link" onClick={handleRedirectLogin}>Login</a>
                         </div>
 
-                    </aside>
-                    <section>
-                        <label htmlFor="password">
-                            Password:
-                            <span className={validPwd ? 'valid' : 'hide'}>
-                                <FaCheck />
-                            </span>
-                            <span className={validPwd || !pwd ? 'hide' : 'invalid'}>
-                                <FaTimes />
-                            </span>
-                        </label>
+                    </form>
 
-                        <input
-                            type="password"
-                            id="password"
-                            onChange={(e) => setPwd(e.target.value)}
-                            required
-                            aria-invalid={validPwd ? "false" : "true"}
-                            aria-describedby="pwdnote"
-                            onFocus={() => setPwdFocus(true)}
-                            onBlur={() => setPwdFocus(false)}
-                        />
-
-                    </section>
-
-                </div>{/* Password/ */}
-                <div className='input--cont'>{/* Password Match */}
-                <aside id="confirmnote"
-                        className={matchFocus && !validMatch ? "instructions" : "offscreen"}>
-                        <AiOutlineInfoCircle />
-                        Must match the first password input field.
-                    </aside>
-                    <section>
-                    <label htmlFor="confirm_pwd">
-                        Confirm Password:
-                        <span className={validMatch && matchPwd ? 'valid' : 'hide'}>
-                            <FaCheck />
-                        </span>
-                        <span className={validMatch || !matchPwd ? 'hide' : 'invalid'}>
-                            <FaTimes />
-                        </span>
-                    </label>
-                    
-                    <input
-                        type="password"
-                        id="confirm_pwd"
-                        onChange={(e) => setMatchPwd(e.target.value)}
-                        required
-                        aria-invalid={validMatch ? "false" : "true"}
-                        aria-describedby="confirmnote"
-                        onFocus={() => setMatchFocus(true)}
-                        onBlur={() => setMatchFocus(false)}
-                    />
-
-                    </section>
-                   
-                </div>{/* Password Match/ */}
-
-                <div>
-                    <button disabled={!validName || !validPwd || !validMatch ? true : false}>Sign Up</button>
                 </div>
-                <div>
-                    Already registered?<br />
-                    <a onClick={handleRedirectLogin}>Login</a>
-                </div>
-
-            </form>
-
-        </div>
+            )}
+        </>
     )
 }
 
