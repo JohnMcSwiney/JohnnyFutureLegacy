2cca694ad71aa3b0df2e3d7d47e35e616f30f9bf
Johnny Mcswiney <102752692+JohnMcSwiney@users.noreply.github.com>
2023-11-09 16:58:12 -0700

    keeps search  scroll position

--- client/src/components/containers/SearchResultsContainer.jsx
+++ client/src/components/containers/SearchResultsContainer.jsx
@@ -1,30 +1,43 @@
-
 import './style.css';
 import React, { useRef, useEffect } from 'react';
+
 function SearchResultsContainer({ children }) {
-    const elementRef = useRef(null);
+  const elementRef = useRef(null);
 
-    useEffect(() => {
-      const handleScroll = () => {
-        const verticalScrollPosition = elementRef.current.scrollTop;
-  
-        console.log('Vertical Scroll Position:', verticalScrollPosition);
-      };
-  
-      // Attach the scroll event listener
+  useEffect(() => {
+    // Retrieve the stored scroll position from localStorage
+    const storedScrollPosition = localStorage.getItem('searchResultsScrollPosition');
+    const initialScrollPosition = storedScrollPosition ? parseInt(storedScrollPosition, 10) : 0;
+
+    // Set the initial scroll position
+    if (elementRef.current) {
+      elementRef.current.scrollTop = initialScrollPosition;
+    }
+
+    const handleScroll = () => {
+      // Save the current scroll position to localStorage
+      localStorage.setItem('searchResultsScrollPosition', elementRef.current.scrollTop);
+    };
+
+    // Attach the scroll event listener
+    if (elementRef.current) {
       elementRef.current.addEventListener('scroll', handleScroll);
-  
-      // Clean up the event listener when the component unmounts
-      return () => {
-        
-      };
-    }, []); // Empty dependency array ensures that the effect runs only once on mount
-  
-    return (
-      <div ref={elementRef} className='search--results--cont'>
-        {children}
-      </div>
-    );
-  }
+    }
 
-export default SearchResultsContainer
+    // Clean up the event listener when the component unmounts
+    return () => {
+      // Check if elementRef.current is not null before trying to remove the event listener
+      if (elementRef.current) {
+        elementRef.current.removeEventListener('scroll', handleScroll);
+      }
+    };
+  }, []); // Empty dependency array ensures that the effect runs only once on mount
+
+  return (
+    <div ref={elementRef} className='search--results--cont'>
+      {children}
+    </div>
+  );
+}
+
+export default SearchResultsContainer;
