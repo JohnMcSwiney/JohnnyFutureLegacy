c82771f187de86171c349af7b91638224607be1a
Johnny Mcswiney <102752692+JohnMcSwiney@users.noreply.github.com>
2023-12-15 15:28:44 -0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,15 +6,13 @@ const app = express();
 
 // Load environment variables from .env file
 require('dotenv').config();
-// /etc/letsencrypt/archive/futurelegacy.mcswineyprogramming.net/cert1.pem
-// /etc/letsencrypt/archive/futurelegacy.mcswineyprogramming.net/privkey1.pem
+
 // Read certificate and private key files
-const privateKey = fs.readFileSync('/etc/letsencrypt/archive/futurelegacy.mcswineyprogramming.net/cert1.pem', 'ASCII');
-const certificate = fs.readFileSync('/etc/letsencrypt/archive/futurelegacy.mcswineyprogramming.net/privkey1.pem', 'ASCII');
+const privateKey = fs.readFileSync('/etc/letsencrypt/archive/futurelegacy.mcswineyprogramming.net/privkey1.pem', 'ASCII');
+const certificate = fs.readFileSync('/etc/letsencrypt/archive/futurelegacy.mcswineyprogramming.net/cert1.pem', 'ASCII');
 
 const credentials = { key: privateKey, cert: certificate };
 
-console.log(credentials);
 // Define your Express routes and middleware here
 
 // Example route
