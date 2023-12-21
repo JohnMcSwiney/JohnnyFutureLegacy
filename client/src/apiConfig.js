3b666e551a8249539ac97001d9d54651c2b06f4e
Johnny Mcswiney <102752692+JohnMcSwiney@users.noreply.github.com>
2023-12-21 13:40:36 -0700

    Update apiConfig.js

--- client/src/apiConfig.js
+++ client/src/apiConfig.js
@@ -1,5 +1,6 @@
 // Amazon ec2 server ip: http://3.134.100.195:5000
 // Amazon ec2 server ip: https://3.134.100.195:443
-// 
-const API_BASE_URL = 'https://futurelegacy.mcswineyprogramming.net/';
+// Amazon ec2 v3: https://3.140.207.175:5000/
+// hostname? idfk https://futurelegacy.mcswineyprogramming.net/
+const API_BASE_URL = 'https://3.140.207.175:5000/';
 export default API_BASE_URL;
