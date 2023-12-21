32a56ed873d77e57340fc02837fda449f67965e5
Johnny Mcswiney <102752692+JohnMcSwiney@users.noreply.github.com>
2023-12-21 14:38:16 -0700

    Update loginPage.jsx

--- client/src/pages/zz-other/register/login/loginPage.jsx
+++ client/src/pages/zz-other/register/login/loginPage.jsx
@@ -31,7 +31,7 @@ export default function LoginPage() {
     }
 
     try {
-      const response = await fetch('/api/auth/login', {
+      const response = await fetch(`${API_BASE_URL}/api/auth/login`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
